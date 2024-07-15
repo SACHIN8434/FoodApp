243668c0df43b20230334e828e430abb9b20c302
SACHIN8434 <sachin843413@gmail.com>
2024-07-15 12:22:02 +0530

    creating folder Structure

---
+++ src/App.js
@@ -0,0 +1,7 @@
+// Lecture->05
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// const h1 = React.createElement('h1',{},"hello from react");
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(h1);
+
